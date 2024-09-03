f75efa4159ac9967d85d4ce3eea60046592617dd
Michael Lei <michaelleie0ca@gmail.com>
2024-09-02 19:17:01 -0700

    Change logo

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,16 +1,19 @@
 import Link from "next/link";
-import Image from "next/image";
+import { Palette } from "lucide-react";
 
 export function Logo() {
   return (
     <Link href="/">
-      <div className="relative size-8 shrink-0">
+      {/* <div className="relative size-8 shrink-0">
         <Image
           src="/logo.svg"
           alt="TheDrawingBoard Logo"
           fill
           className="shrink-0 transition hover:opacity-75"
         />
+      </div> */}
+      <div className="ml-2 rounded-full bg-gradient-to-r from-pink-500 to-violet-600 p-2 text-background">
+        <Palette className="shrink-0" />
       </div>
     </Link>
   );
