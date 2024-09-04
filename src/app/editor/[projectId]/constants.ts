df994e23019b548f2d60d8662f5d6371a60ddf5c
Michael Lei <michaelleie0ca@gmail.com>
2024-09-03 21:55:34 -0700

    Add selection dependent tools

--- src/app/editor/[projectId]/constants.ts
+++ src/app/editor/[projectId]/constants.ts
@@ -1,3 +1,15 @@
-export const FILL_COLOR = "rgba(0, 0, 0, 1)";
+import type { ActiveTool } from "./types";
+
+export const FILL_COLOR = "rgba(255, 255, 255, 1)";
 export const STROKE_COLOR = "rgba(0, 0, 0, 1)";
-export const STROKE_WIDTH = 2;
+export const STROKE_WIDTH = 4;
+
+export const SELECTION_DEPENDENT_TOOLS: ActiveTool[] = [
+  "fill",
+  "font",
+  "filter",
+  "opacity",
+  "remove-bg",
+  "stroke-color",
+  "stroke-width",
+] as const;
