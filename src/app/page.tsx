650f27290bbb03553610f8e501b52b48f2debac7
Michael Lei <michaelleie0ca@gmail.com>
2024-09-16 20:25:10 -0700

    Add authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,26 @@
+import { auth } from "@/auth";
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth();
+  const isAuthenticated = !!session;
   return (
-    <div>
-      <Button>Click me</Button>
+    <div className="p-4">
+      {isAuthenticated ? (
+        <div className="w-96">
+          <code>{JSON.stringify(session, null, 2)}</code>
+          <div className="mt-4">
+            <Button asChild>
+              <Link href="/api/auth/signout">Logout</Link>
+            </Button>
+          </div>
+        </div>
+      ) : (
+        <Button asChild>
+          <Link href="/api/auth/signin">Login</Link>
+        </Button>
+      )}
     </div>
   );
 }
