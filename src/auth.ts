9be0747f0eb8a0241063d7ac4cf8ad6c69ae3954
Michael Lei <michaelleie0ca@gmail.com>
2024-09-18 23:32:56 -0700

    Add google login

--- src/auth.ts
+++ src/auth.ts
@@ -1,9 +1,14 @@
 import NextAuth from "next-auth";
 import Discord from "next-auth/providers/discord";
+import Google from "next-auth/providers/google";
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import { db } from "@/db";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: DrizzleAdapter(db),
-  providers: [Discord],
+  providers: [Discord, Google],
+  pages: {
+    signIn: "/login",
+    error: "/login",
+  },
 });
