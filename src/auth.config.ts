9cd27c4604ad33799c6809fab9ca50d9750ab4c4
Michael Lei <michaelleie0ca@gmail.com>
2025-05-19 10:01:27 -0700

    Whoops

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -2,6 +2,7 @@ import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import type { NextAuthConfig } from "next-auth";
 import Discord from "next-auth/providers/discord";
 import Google from "next-auth/providers/google";
+
 import { db } from "@/db";
 export default {
   adapter: DrizzleAdapter(db),
