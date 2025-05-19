d184331691d39f95960dc6a8b79149efb3e25c1c
Michael Lei <michaelleie0ca@gmail.com>
2025-05-19 10:12:06 -0700

    Remove banner

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,5 +1,4 @@
 import { protectServer } from "@/app/(auth)/utils";
-import Banner from "./_components/banner";
 import ProjectsSection from "./_components/projects-section";
 import TemplateSection from "./_components/template-section";
 
@@ -7,7 +6,6 @@ export default async function Home() {
   await protectServer();
   return (
     <div className="mx-auto flex flex-col space-y-8">
-      <Banner />
       <TemplateSection />
       <ProjectsSection />
     </div>
