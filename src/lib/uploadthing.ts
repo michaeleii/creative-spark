2afe890ce0abd1b18a7b9d165229d3748ee99138
Michael Lei <michaelleie0ca@gmail.com>
2025-05-22 02:20:08 -0700

    Fix upload button

--- src/lib/uploadthing.ts
+++ src/lib/uploadthing.ts
@@ -3,5 +3,6 @@ import {
   generateUploadDropzone,
 } from "@uploadthing/react";
 import type { OurFileRouter } from "@/app/api/uploadthing/core";
+import "@uploadthing/react/styles.css";
 export const UploadButton = generateUploadButton<OurFileRouter>();
 export const UploadDropzone = generateUploadDropzone<OurFileRouter>();
